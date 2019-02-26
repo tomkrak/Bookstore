7e532416433c8970a275d698790f7451aad71771
tomkrak <tomasz.krakowiak@outlook.com>
2019-02-26 21:45:41 +0100

    po sugerowanych poprawkach trenera

--- src/main/java/bookstore/Main.java
+++ src/main/java/bookstore/Main.java
@@ -12,72 +12,69 @@ class Main {
         getData();
     }
 
-    private static void readBook(String plik1) throws IOException {
-        File file = new File(plik1);
-        List<String> list = new ArrayList<>();
+    private static void readBook(String fileBooks) throws IOException {
+        File file = new File(fileBooks);
+        List<String> forAdding = new ArrayList<>();
         if (file.exists()) {
             InputStream is = new FileInputStream(file);
             InputStreamReader isr = new InputStreamReader(is);
             BufferedReader reader = new BufferedReader(isr);
             while (reader.ready()) {
-                list.add(reader.readLine());
+                forAdding.add(reader.readLine());
             }
             reader.close();
             isr.close();
             is.close();
         }
         List<Book> books = new ArrayList<>();
-
-        for (String line : list) {
+        for (String line : forAdding) {
             String[] split = line.split(";");
             Book book = new Book(Integer.parseInt(split[0]), split[1],
                     Integer.parseInt(split[2]), Integer.parseInt(split[3]), split[4], null, null);
             books.add(book);
         }
         System.out.println(books);
-        list.toString();
     }
 
-    private static void readAuthor(String plik2) throws IOException {
-        File file = new File(plik2);
-        List<String> list = new ArrayList<>();
+    private static void readAuthor(String fileAuthors) throws IOException {
+        File file = new File(fileAuthors);
+        List<String> forAdding = new ArrayList<>();
         if (file.exists()) {
             InputStream is = new FileInputStream(file);
             InputStreamReader isr = new InputStreamReader(is);
             BufferedReader reader = new BufferedReader(isr);
             while (reader.ready()) {
-                list.add(reader.readLine());
+                forAdding.add(reader.readLine());
             }
             reader.close();
             isr.close();
             is.close();
         }
         List<Author> authors = new ArrayList<>();
-        for (String line : list) {
+        for (String line : forAdding) {
             String[] split = line.split(";");
             Author author = new Author(Integer.parseInt(split[0]), split[1], Integer.parseInt(split[2]));
             authors.add(author);
         }
         System.out.println(authors);
-        list.toString();
     }
 
-    private static void readCategory(String plik3) throws IOException {
-        File file = new File(plik3);
-        List<String> list = new ArrayList<>();
+    private static void readCategory(String fileCategories) throws IOException {
+        File file = new File(fileCategories);
+        List<String> forAdding = new ArrayList<>();
         if (file.exists()) {
             InputStream is = new FileInputStream(file);
             InputStreamReader isr = new InputStreamReader(is);
             BufferedReader reader = new BufferedReader(isr);
             while (reader.ready()) {
-                list.add(reader.readLine());
+                forAdding.add(reader.readLine());
             }
             reader.close();
             isr.close();
             is.close();
         }
         List<Category> categories = new ArrayList<>();
-        for (String line : list) {
+        for (String line : forAdding) {
             String[] split = line.split(";");
             Category category = new Category(Integer.parseInt(split[0]), split[1], Integer.parseInt(split[2]));
             categories.add(category);
@@ -89,7 +86,6 @@ class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Wybierz opcję z Menu");
         int liczba = scanner.nextInt();
-
         while (liczba != 4) {
             if (liczba == 1) {
                 readBook("C:\\Users\\Tomasz.Dorotka-VAIO\\IdeaProjects\\Bookstore\\books.csv");
